526a3328ba42dab113c5989c1e3b3011f9943dbc
Andre Srinivasan <andre@redislabs.com>
2019-09-26 08:16:24 -0700

    Correct for int/float bug
    Signed-off-by: Andre Srinivasan <andre@redislabs.com>

--- edge-ts/gears/ts-pipeline.py
+++ edge-ts/gears/ts-pipeline.py
@@ -1,25 +1,48 @@
 def addToTS(r):
-    # Intellicode doesn't know about execute
+    execute("xadd", "log", "*", "f", "addToTS", "enter", r)
+
     execute("TS.ADD", "temp:raw", "*", r["t"])
     return r
 
 
-def calcSMA(r):
-    avgs = execute("TS.RANGE", "temp:avg:1s", 0, -1)
-    execute("XADD", "log", "*", "f", "calcSMA", "avgs", avgs, "sa len", len(avgs))
+def calcSMA(ignore):
+    try:
+        execute("xadd", "log", "*", "f", "calcSMA", "enter", ignore)
 
-    sma = sum([int(x[1]) for x in avgs]) / len(avgs)
-    execute("XADD", "log", "*", "f", "calcSMA", "sma", sma)
+        avgs = execute("TS.RANGE", "temp:avg:1s", 0, -1)
+
+        execute("xadd", "log", "*", "f", "calcSMA", "avgs", avgs, "sa len", len(avgs))
+
+        sma = -1
+        if len(avgs) > 0:
+            sma = sum([float(x[1]) for x in avgs]) / len(avgs)
+
+        execute("xadd", "log", "*", "f", "calcSMA", "sma", sma)
+
+    except Exception as e:
+        execute("xadd", "log", "*", "f", "calcSMA", "EXCEPTION", e)
 
     return sma
 
 
-def publish(r):
-    execute("XADD", "egress", "*", "sma", r)
+def noValue(sma):
+    execute("xadd", "log", "*", "f", "noValue", "enter", sma)
+
+    return sma > -1
 
 
-gb = gearsCtx("StreamReader").map(addToTS).map(calcSMA).map(publish)
+def publish(sma):
+    execute("xadd", "log", "*", "f", "publish", "enter", sma)
 
+    execute("xadd", "egress", "*", "sma", sma)
+
+
+gb = GearsBuilder("StreamReader").map(addToTS).map(calcSMA).filter(noValue).map(publish)
 gb.register("ingress")
 
 execute("hmset", "gears-status", "name", "ts-pipeline", "status", "loaded")
+
+
+# Questions
+# Can I have a redis symbol so I can use redis.hmset directly?
+# Can I have a GearsBuilder symbol...
